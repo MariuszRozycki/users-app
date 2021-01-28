6e2a8f9dece30de6a74d183562686c58b5cb56ea
mariansc83 <mariansc83@icloud.com>
2021-01-28 14:46:05 +0100

    Add ready users-app

--- src/Users.js
+++ src/Users.js
@@ -4,16 +4,55 @@ import UsersList from './UsersList';
 
 class Users extends Component {
 
+  constructor(props) {
+    super(props);
+
+    this.state ={
+      users: []
+    };
+  }
+
+  addUser = (event) => {
+    event.preventDefault();
+    
+
+    let newUser = {
+      id: Date.now(),
+      name: this._inputName.value
+    }
+
+    console.log(newUser);
+    console.log(newUser.name);
+
+    this.setState((state) => {
+
+      return({
+        users: state.users.concat(newUser)
+      });
+    })
+
+    this._inputName.value = '';
+  } 
+
+  removeUser = (userID) => {
+    console.log('userID', userID);
+    console.log('this.removeUser');
+    this.setState(state => {
+      return({
+        users: state.users.filter(user => user.id !== userID)
+      });
+    })
+  }
 
     render() {
       return(
         <div className="users-main">
           <h1>User's List</h1>
-          <form>
-            <input type="text" placeholder="Enter name"/>
+          <form onSubmit={this.addUser}>
+            <input ref={(element) => {this._inputName = element;}} type="text" placeholder="Enter name"/>
             <button type="submit">Add user</button>
           </form>
-          <UsersList />
+          <UsersList usersList={this.state.users} removeUserMethod={this.removeUser} />
         </div>
       );
     }
--- src/UsersList.js
+++ src/UsersList.js
@@ -3,10 +3,16 @@ import './UsersList.css';
 
 function UsersList(props) {
 
+  let usersList = props.usersList;
+  let usersLiElements = usersList.map((user) => {
+    return <li key={user.id}>{user.name} <span onClick={() => props.removeUserMethod(user.id)}>X</span></li>
+  })
+  console.log(usersList);
+  console.log(usersLiElements);
+
   return(
     <ul className="the-list">
-      <li>Hans</li>
-      <li>Mariusz</li>
+      {usersLiElements}
     </ul>
   )
 }
